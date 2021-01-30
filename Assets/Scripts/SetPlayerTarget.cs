298e45317605d3dfd228d048808e3df50a8000f6
Luiz Fernando <luiz.lira@fatec.sp.gov.br>
2021-01-30 15:13:05 -0300

    progresso temporário com coisas que vão ser deletadas depois

---
+++ Assets/Scripts/SetPlayerTarget.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RDG;
+using Pathfinding;
+using TMPro;
+using UnityEngine;
+
+public class SetPlayerTarget : MonoBehaviour{
+    [Range(0,1)]
+    public float minDistance = 0.2f;
+    [Header("Objeto a ser referenciado")]
+    public Transform targetPoint;
+    [Header("AI controller")]
+    public AIDestinationSetter targetController;
+    [SerializeField]
+    private InteractableTarget interactableObject;
+
+    [SerializeField] private GameObject textArea;
+    [SerializeField] private TextMeshPro textToShow;
+    //private Animator robotAnimator;
+    void Start(){
+        targetController = GetComponent<AIDestinationSetter>();
+        targetController.enabled = false;
+        interactableObject = null;
+        textArea.SetActive(false);
+        //robotAnimator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetMouseButton(0)){
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = Camera.main.nearClipPlane;
+            targetPoint.position = Camera.main.ScreenToWorldPoint(mousePosition);
+            targetController.target = targetPoint;
+            targetController.enabled = true;
+            interactableObject = null;
+            //robotAnimator.SetBool("Walking", true);
+            transform.localScale = new Vector3(Math.Sign(targetPoint.position.x - transform.position.x), 1, 1);
+            textArea.transform.localScale = transform.localScale = new Vector3(Math.Sign(targetPoint.position.x - transform.position.x), 1, 1);
+            RaycastHit2D hit = Physics2D.Raycast(targetPoint.position, -Vector2.up, 0.05f);
+            if (hit.collider != null){
+                if (hit.transform.gameObject.GetComponent<InteractableTarget>()){
+                    interactableObject = hit.transform.gameObject.GetComponent<InteractableTarget>();
+                }
+            }
+        }
+
+        float newSize = (4 - transform.position.y);
+        //transform.localScale = new Vector3(Math.Abs(newSize), newSize);
+    }
+
+    private void FixedUpdate(){
+        if (Vector2.Distance(transform.position, targetPoint.position) <= minDistance){
+            targetController.enabled = false;
+            //robotAnimator.SetBool("Walking", false);
+            if (interactableObject != null){
+                Vibration.Vibrate(100, 100);
+                textToShow.text = TextController.getString("TEXT_EXAMPLE_1");
+                textArea.SetActive(true);
+                interactableObject = null;
+                StartCoroutine(nameof(HideTextArea));
+            }
+        }
+    }
+
+    public IEnumerator HideTextArea(){
+        yield return new WaitForSeconds(5);
+        textArea.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TextControllers/TextController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+using fastJSON;
+
+public class TextController : MonoBehaviour{
+    static TextController _instance;
+    public static TextController instance{ get{ return _instance; } }
+
+    [SerializeField] private Object languageFile;
+    [SerializeField] private Dictionary<string, string> textStrings;
+    [SerializeField] private string languageId, loadedLanguage = "ptBR";
+    [SerializeField] private bool languageChanged{ get{ return loadedLanguage != languageId; }}
+
+    public void Awake(){
+        if (_instance == null) {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }else if (_instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Start(){
+        LoadLanguage(languageId);
+    }
+
+    public void LoadLanguage(string languageCode){
+        languageFile = Resources.Load("language/" + languageCode);
+        textStrings = JSON.ToObject<Dictionary<string,string>>(languageFile.ToString());
+        loadedLanguage = languageCode;
+    }
+
+
+    public static string getString(string stringId){
+        if(_instance.languageChanged) _instance.LoadLanguage(_instance.languageId);
+        string text = stringId;
+        _instance.textStrings.TryGetValue(stringId, out text);
+        return text;
+    }
+}
