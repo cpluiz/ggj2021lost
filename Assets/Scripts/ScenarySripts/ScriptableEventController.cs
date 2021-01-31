0a1a0c5d133aee246825bbf2bc62d321ab0c3201
Luiz Fernando <luiz.lira@fatec.sp.gov.br>
2021-01-31 14:44:24 -0300

    preparing to receive commit

--- Assets/Scripts/ScenarySripts/ScriptableEventController.cs
+++ Assets/Scripts/ScenarySripts/ScriptableEventController.cs
@@ -50,6 +50,35 @@ public class ScriptableEventController : MonoBehaviour{
                 while (!Speaker.completed){
                     yield return new WaitForEndOfFrame();
                 } 
+            }else if (currentSequence.segment[sequenceStep].type == SegmentType.ambience){
+                switch (currentSequence.segment[sequenceStep].ambienceEffect){
+                    case AmbienceEffectType.vignetteTotalGloom:
+                        ClimateChangeController.VignetteTotalGloom();
+                        break;
+                    case AmbienceEffectType.vignetteNormal:
+                        ClimateChangeController.VignetteToBrighter();
+                        break;
+                    case AmbienceEffectType.vignetteGloom:
+                        ClimateChangeController.VignetteToGloomer();
+                        break;
+                    case AmbienceEffectType.colorGloom:
+                        ClimateChangeController.ChangeSceneColorToGloomer();
+                        break;
+                    case AmbienceEffectType.colorNormal:
+                        ClimateChangeController.ChangeSceneColorToNormal();
+                        break;
+                    case AmbienceEffectType.temperatureGloom:
+                        ClimateChangeController.ChangeSceneTemperatureToCold();
+                        break;
+                    case AmbienceEffectType.temperatureNormal:
+                        ClimateChangeController.ChangeSceneTemperatureToNormal();
+                        break;
+                    default:
+                        ClimateChangeController.VignetteToBrighter();
+                        ClimateChangeController.ChangeSceneTemperatureToNormal();
+                        ClimateChangeController.ChangeSceneColorToNormal();
+                        break;
+                }
             }
             sequenceStep++;
         }
