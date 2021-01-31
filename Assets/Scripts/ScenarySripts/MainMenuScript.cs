87f166313c1752416f861fa5f20c50e79f984794
Luiz Fernando <luiz.lira@fatec.sp.gov.br>
2021-01-31 04:55:50 -0300

    merged in merged branch of merged things

---
+++ Assets/Scripts/ScenarySripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour{
+
+    public void NewGame(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Settings(){
+        Debug.Log("Settings here");
+    }
+
+    public void Credits(){
+        Debug.Log("Credits here \\o>");
+    }
+
+    public void CloseGame(){
+        Application.Quit();
+    }
+}
